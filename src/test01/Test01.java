d442423eede4bd7fd21ab32a3105a8448ae91575
“““fushaohu <814015115@qq.com>
2020-03-17 00:20:37 +0800

    resolved a conflect

--- src/test01/Test01.java
+++ src/test01/Test01.java
@@ -2,12 +2,6 @@ package test01;
 
 public class Test01 {
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("你好，周北婴同学！你好吗？");
-        System.out.println("hello,word,世界，你好！");
-=======
-        System.out.println("你好，周北婴同学！");
-        System.out.println("我是你未来的好朋友！");
->>>>>>> 1c0c48016f98f5a9a982840defade83a92947aa4
     }
 }
