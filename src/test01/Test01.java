ac6f069d89290634e8dfe07dc1692f239ed491bd
“““fushaohu <814015115@qq.com>
2020-03-17 00:02:25 +0800

    modified a file

--- src/test01/Test01.java
+++ src/test01/Test01.java
@@ -2,6 +2,6 @@ package test01;
 
 public class Test01 {
     public static void main(String[] args) {
-        System.out.println("hello,word");
+        System.out.println("hello,word,世界，你好！");
     }
 }
